805136b3bc6da75711742858ac75b757df230da0
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-04-25 17:56:41 +0200

    create nav

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ mongoose.connect(process.env.MONGODB_URI, {
     app.use(express.json());
     app.use("/api", router)
     app.use(favicon(__dirname + "/build/favicon.ico"))
-    app.use(express.static(path.join(__dirname, "build")))
+    app.use(express.static(path.join(__dirname, "../client/build")))
     app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/build/index.html"))
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
     const herokuPort = process.env.PORT || 3000
     app.listen(herokuPort, ()=>{
