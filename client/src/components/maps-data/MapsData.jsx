148414e3935425803cc69e44a21f4c4e1d6d4122
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-04-25 21:43:14 +0200

    leaflet add

--- client/src/components/maps-data/MapsData.jsx
+++ client/src/components/maps-data/MapsData.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet"
+import { Icon } from "leaflet";
 import "./mapsData.css"
-
+const position = [51.2181945, 22.5546776]
 const MapsData = props =>{
 
-
     return(
-        <div className="mapsDataContainer">
-            <h2>pokazemy mapy</h2>
-        </div>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+             />
+             <Marker position={position}>
+                 <Popup>Testowy popup</Popup>
+             </Marker>
+            </MapContainer>
+        
     )
 }
 
