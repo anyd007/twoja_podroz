9743d8dbf3a0283edf83484957bed8c01ee6d371
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-04-26 16:13:42 +0200

    adding inpunt for tripadd

--- client/src/components/trip-add/TripAdd.jsx
+++ client/src/components/trip-add/TripAdd.jsx
@@ -7,7 +7,31 @@ const TripAdd = props =>{
 
     return(
         <div className="tripAddContainer">
-            <h2>wyznaczamy trase</h2>
+            <div className="background"></div>
+            <section className="titleContener">
+                <h2>WYZNACZ SWOJĄ TRASĘ</h2>
+            </section>
+            <section className="mainContener">
+              <div className="inpuntDiv">
+                <label htmlFor="startTrip">PODAJ START TRASY</label><br />
+                <input 
+                className="tripInputs"
+                type="text" 
+                name="startTrip"
+                />
+              </div>
+              <div className="inpuntDiv">
+                <label htmlFor="finishTrip">PODAJ PUNKT DOCELOWY</label><br />
+                <input
+                className="tripInputs"
+                type="text"
+                name="finishTrip"
+                />
+              </div>
+                <div className="tripBtnDiv">
+                    <button type="button" className="btn">WYZNACZ</button>
+                </div>
+            </section>
         </div>
     )
 }
